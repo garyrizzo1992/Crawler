95e981d9b7f56ca8c9949a1dcfbddaec1e6179ea
gary <garyrizzo@hotmail.com>
2017-01-09 21:08:16 +0100

    Imports

--- Ferrara.py
+++ Ferrara.py
@@ -1,2 +1,28 @@
-import bs4
+from bs4 import BeautifulSoup
 import responses
+import urllib2
+import re
+
+url = "http://www.ferraramalta.com/"
+page = urllib2.urlopen(url)
+
+soup = BeautifulSoup(page,"html.parser")
+
+
+print soup.title.string
+
+
+all_links = soup.find('ul', class_='level1')
+
+all_links2 = all_links.findAll('a', attrs={'href': re.compile("^http://")})
+
+#print str(all_links.prettify())
+
+for link in all_links2:
+        try:
+            print link.text
+            print link.get("href")
+        except Exception as ex:
+            print ex
+
+#print soup.prettify()
